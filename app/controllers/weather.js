4d9496a9983a2cba24a8d9dd219c2b09aba145e6
Luca Santarella <luca.santarella@gmail.com>
2017-10-19 14:16:06 -0400

    new name

--- app/controllers/weather.js
+++ app/controllers/weather.js
@@ -5,23 +5,20 @@ const express = require('express'),
 const WeatherBaseUrl = 'http://api.openweathermap.org/data/2.5/';
 const API_KEY = '602d10eaa16056b9fe71e9d6693e44db';
 
-const WeatherBaseUrl = 'http://api.openweathermap.org/data/2.5/';
-const API_KEY = '602d10eaa16056b9fe71e9d6693e44db';
-
 module.exports = function (app) {
   app.use('/weather', router);
 };
 
-router.get('/:zip_code', function (req, res, next) {
+router.get('/:zipCode', function (req, res, next) {
 
-  let zipCode = req.params.zip_code;
+  let zipCode = req.param('zipCode');
 
   request(WeatherBaseUrl + 'weather?zip=' + zipCode + ',us&appid=' + API_KEY,
     function (error, response, body) {
-      let httpresponse = JSON.parse(body);
-      let weatherObj = httpresponse.weather[0];
+      let apiResponse = JSON.parse(body);
+      let weatherObj = apiResponse.weather[0];
       res.render('weather', {
-        title: 'Weather for ' + zipCode,
+        title: 'Weather for ' + apiResponse.name,
         imgicon: 'http://openweathermap.org/img/w/' + weatherObj.icon + '.png',
         weather: weatherObj.description
       });
